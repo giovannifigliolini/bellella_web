af1bda422a91fe88a42e14f13ff9cff35008a11f
giovanni <g.figliolini@almaviva.it>
2024-03-27 12:03:41 +0100

    images + trattoria page

--- src/app/trattoria/trattoria.component.ts
+++ src/app/trattoria/trattoria.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrattoriaComponent implements OnInit {
 
+  portfolio = [
+    {
+      path: 'assets/img/trattoria/trattoria_1.jpeg',
+    },
+    {
+      path: 'assets/img/trattoria/trattoria_2.jpeg',
+    },
+    {
+      path: 'assets/img/trattoria/trattoria_3.jpeg',
+    },
+    {
+      path: 'assets/img/trattoria/trattoria_4.jpeg',
+    },
+    {
+      path: 'assets/img/trattoria/trattoria_5.jpeg',
+    },
+    {
+      path: 'assets/img/trattoria/trattoria_6.jpeg',
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
