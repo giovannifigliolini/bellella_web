79fa8d8555aa7931cad90e0d7c5a345724de4da1
alex-mele <alex-mele@hotmail.it>
2024-03-27 14:48:51 +0100

    Prime modifiche foto + descrizione +link

--- src/app/pizzeria/pizzeria.component.ts
+++ src/app/pizzeria/pizzeria.component.ts
@@ -7,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzeriaComponent implements OnInit {
 
+  portfolio = [
+    {
+      path: 'assets/img/trattoria/trattoria_1.jpeg',
+    },
+    {
+      path: 'assets/img/trattoria/trattoria_2.jpeg',
+    },
+    {
+      path: 'assets/img/trattoria/trattoria_3.jpeg',
+    },
+    {
+      path: 'assets/img/trattoria/trattoria_4.jpeg',
+    },
+    {
+      path: 'assets/img/trattoria/trattoria_5.jpeg',
+    },
+    {
+      path: 'assets/img/trattoria/trattoria_6.jpeg',
+    },
+  ];
+
+  descrizione1 = `Bellella è il frutto dell'idea di due giovani ragazzi, Raffaele Marigliano e Giorgio Riccio, che hanno voluto coniugare la bellezza e la bontà della nostra terra. Situata in un'atmosfera calda e accogliente, ispirata alla Napoli antica, qui potrai trascorrere i momenti migliori in tutta tranquillità e spensieratezza. Il nostro team ti accoglierà sempre con il sorriso, sia a pranzo che a cena.`;
+
+  descrizione2 = `Bellella Pizzeria Napoletana ® offre una Pizza Contemporanea realizzata con una selezione accurata di grano e un blend di farine di TIPO “0” e TIPO “1”. La nostra lievitazione, che va dalle 24 alle 36 ore, assicura un prodotto estremamente digeribile, soffice e leggero. Un continuo omaggio al territorio napoletano tramite i suoi prodotti e le sue tradizioni, reinterpretate in chiave moderna.`;
+
+  descrizione3 = `Da noi potrai gustare l'autentica Pizza Napoletana, preparata con ingredienti di altissima qualità. Grazie alla nostra attenta e costante ricerca delle migliori materie prime italiane, incluse quelle con certificazione DOP, IGP e SLOW FOOD. Inoltre, oltre alla pizza, troverai una vasta selezione di fritti napoletani rivisitati in chiave moderna, tra cui la nostra celebre "frittatina alla carbonara", diventata nel tempo il nostro fritto più richiesto.`;
+
+  descrizione4 = `Ci impegniamo a selezionare ingredienti freschi, genuini e di alta qualità. La nostra location è dotata di una camera di lievitazione con temperatura controllata, dove il nostro impasto, preparato con metodo Biga e una miscela di farine, lievita per almeno 48 ore. Grazie alla tecnologia avanzata della camera di lievitazione, otteniamo ogni giorno lo stesso impasto, garantendo la massima qualità in ogni preparazione.`;
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/trattoria/trattoria.component.ts
+++ src/app/trattoria/trattoria.component.ts
@@ -28,6 +28,14 @@ export class TrattoriaComponent implements OnInit {
     },
   ];
 
+  descrizione1 = 'Siete pronti ad entrare in un mondo di Napoletanità e Qualità? Asstatv e facitne salute! Bellella Trattoria Napoletana nasce dalla Passione per la cucina antica Napoletana e dal Fascino dei vicoli di Napoli. Bellella è progettata con un Design caratteristico dei bassi napoletani, pronti a coccolarvi in un\' atmosfera calda e accogliente. La nostra è la tipica cucina della Domenica a casa della Nonna, dalla Genovese al Ragù, dalla Polpetta alla Parmigiana, serviti nei famosi pentolini di rame. BELLELLA vi aspetta tutti i giorni nei suoi locali, a pranzo come a cena.';
+
+  descrizione1foot = 'SCUGNIZZO (O\' scugnizz\') La parola "scugnizzo" indica, dalla fine dell\'Ottocento, il «monello di strada» napoletano, è un termine della lingua napoletana, poi entrato nell\'uso anche in italiano. Gli scugnizzi sono bambini nati e cresciuti nei vicoli dei quartieri, spesso senza famiglia, fuggiti dagli orfanotrofi e dai collegi, ragazzi astuti, intelligenti e vivaci, disposti ad «arrangiarsi» con espedienti anche scarsamente onesti perché la vita, in strada, è fatta di espedienti e sopravvivenza.';
+
+  descrizione2 = `Benvenuto alla Trattoria Napoletana, un luogo magico dove la tradizione e la convivialità si incontrano per offrirti un'esperienza culinaria autentica. Qui potrai gustare i sapori genuini della cucina napoletana, preparata con ingredienti freschi e stagionali, seguendo le ricette tramandate di generazione in generazione direttamente dai quartieri di Napoli. Ogni boccone è un viaggio che ti porta a scoprire i sapori e le tradizioni di questa affascinante città, regalandoti un'esperienza culinaria unica e indimenticabile.`;
+
+  descrizione3 = `Ti invitiamo a entrare nella Trattoria Napoletana, dove l'atmosfera accogliente e familiare ti farà sentire a casa fin dal primo istante. Le pareti della nostra trattoria sono decorate con foto dei quartieri di Napoli, mentre i numeri della tombola aggiungono un tocco di nostalgia e divertimento all'ambiente. Concediti il piacere di immergerti completamente nell'atmosfera vivace e allegra della tradizione napoletana mentre assapori i nostri deliziosi piatti e ti godi la compagnia dei tuoi cari.`;
+  
   constructor() { }
 
   ngOnInit(): void {
